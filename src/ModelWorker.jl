11512cb84941f02c9ff0a8768f1083add0988aac
RalphAmmon <ralph.ammon@googlemail.com>
2023-05-08 05:57:08 +0200

    add PCA to module

--- src/ModelWorker.jl
+++ src/ModelWorker.jl
@@ -1,7 +1,7 @@
 module ModelWorker
-using DataFrames, ODBC, DBInterface, Preferences, GLM
+using DataFrames, ODBC, DBInterface, Preferences, MultivariateStats, GLM
 
-export my_function, connectDB, fitModels, closeDB, runFitter
+export my_function, connectDB, fitModels, closeDB, runFitter, doPCA
 
 function my_function(x)
     return 2 * x
@@ -27,6 +27,9 @@ function runFitter(conn::DBInterface.Connection)
             if job.Task == "Fit"
                 println("Fit $modelQueueID")
                 fitModels(conn, job)
+            elseif job.Task == "PCA"
+                println("doPCA")
+                doPCA(conn, job)
             else
                 doLoop = false
                 println("doLoop = $doLoop")
@@ -34,6 +37,37 @@ function runFitter(conn::DBInterface.Connection)
             DBInterface.execute(conn, "UPDATE ModelQueue SET FitTS = Now() WHERE (ModelQueueID=$modelQueueID);")
         end
         sleep(1.0)
+    end
+end
+function doPCA(conn::DBInterface.Connection, job::DataFrameRow)
+    pcaConfig = DBInterface.execute(conn, "SELECT * FROM PCA_Config WHERE (PCA_ConfigID=$(job.PCA_ConfigID));") |> DataFrame
+    configRow = first(pcaConfig)
+    configID = configRow.PCA_ConfigID
+    trainingHorizon = configRow.TrainingHorizonID
+    DBInterface.execute(conn, "UPDATE CurrentTrainingHorizon SET TrainingHorizonID = $(configRow.TrainingHorizonID)")
+    predictionHorizon = configRow.PredictionHorizonID
+    DBInterface.execute(conn, "UPDATE CurrentPredictionHorizon SET PredictionHorizonID = $(configRow.PredictionHorizonID)")
+
+
+    # load Tariffs
+    tariffs = DBInterface.execute(conn, "SELECT * FROM TariffsKT") |> DataFrame
+
+    # traning set tariffs
+    xTariffs = convert(Matrix{Float64}, Matrix(tariffs[:, 2:end])')
+
+    # train a PCA tariff model, allowing up to 3 dimensions
+    pcaModel = fit(PCA, xTariffs; maxoutdim=3)
+
+    # predict principle components
+    tariffs2Predict = DBInterface.execute(conn, "SELECT * FROM Tariffs2PredictKT") |> DataFrame
+    pTariffs = convert(Matrix{Float64}, Matrix(tariffs2Predict[:, 2:end])')
+    tariffsPCA = DataFrame(hcat(tariffs.PeriodID, predict(pcaModel, pTariffs)'), [:PeriodID, :PC1, :PC2, :PC3])
+
+
+    DBInterface.execute(conn, "Delete * FROM TariffsPCA WHERE (PCA_ConfigID=$configID)")
+
+    for row in eachrow(tariffsPCA)
+        DBInterface.execute(conn, "INSERT INTO TariffsPCA ( PCA_ConfigID, PeriodID, PC1, PC2, PC3) values($configID, $(row.PeriodID), $(row.PC1), $(row.PC2), $(row.PC3))")
     end
 end
 
@@ -67,7 +101,7 @@ function fitModels(conn::DBInterface.Connection, job::DataFrameRow)
 
     for group in groups
         groupID = group.ModelID[1]
-        println(groupID)
+        # println(groupID)
 
         modelVector = fill(groupID, coefSize)
         myLM = glm(modelFormula, group, Normal(), IdentityLink(), wts=convert(Vector{Float64}, group[:, weightField]))
