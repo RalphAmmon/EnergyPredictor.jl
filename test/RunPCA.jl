e831185d353952afd37d081743e717250293e9c3
RalphAmmon <ralph.ammon@googlemail.com>
2023-05-08 05:57:32 +0200

    add PCA to module

--- test/RunPCA.jl
+++ test/RunPCA.jl
@@ -1,14 +1,37 @@
-using MultivariateStats, CSV, DataFrames
+using MultivariateStats, CSV, DataFrames, ModelWorker, DBInterface
+
+conn = ModelWorker.connectDB()
+
+modelWorkQueue = DBInterface.execute(conn, "SELECT * FROM ModelWorkQueue") |> DataFrame
+job = first(modelWorkQueue)
+pcaConfig = DBInterface.execute(conn, "SELECT * FROM PCA_Config WHERE (PCA_ConfigID=$(job.PCA_ConfigID));") |> DataFrame
+configRow = first(pcaConfig)
+configID = configRow.PCA_ConfigID
+trainingHorizon = configRow.TrainingHorizonID
+DBInterface.execute(conn, "UPDATE CurrentTrainingHorizon SET TrainingHorizonID = $(configRow.TrainingHorizonID)")
+predictionHorizon = configRow.PredictionHorizonID
+DBInterface.execute(conn, "UPDATE CurrentPredictionHorizon SET PredictionHorizonID = $(configRow.PredictionHorizonID)")
+
 
 # load Tariffs
-tariffs = DataFrame(CSV.File("./data/flat/Tariffs.csv"))
+tariffs = DBInterface.execute(conn, "SELECT * FROM TariffsKT") |> DataFrame
 
 # traning set tariffs
-xTariffs = convert(Array, Matrix(tariffs[:, 2:end]))
+xTariffs = convert(Matrix{Float64}, Matrix(tariffs[:, 2:end])')
 
 # train a PCA tariff model, allowing up to 3 dimensions
 pcaModel = fit(PCA, xTariffs; maxoutdim=3)
 
-uLoad = loadings(pcaModel)
+# predict principle components
+tariffs2Predict = DBInterface.execute(conn, "SELECT * FROM Tariffs2PredictKT") |> DataFrame
+pTariffs = convert(Matrix{Float64}, Matrix(tariffs2Predict[:, 2:end])')
+tariffsPCA = DataFrame(hcat(tariffs.PeriodID, predict(pcaModel, pTariffs)'), [:PeriodID, :PC1, :PC2, :PC3])
 
-tariffsPCA = DataFrame(PC1 = -1.0 * uLoad[:,1],PC2 = -1.0 * uLoad[:,2],PC3 = uLoad[:,3])
+
+DBInterface.execute(conn, "Delete * FROM TariffsPCA WHERE (PCA_ConfigID=$configID)")
+
+for row in eachrow(tariffsPCA)
+    DBInterface.execute(conn, "INSERT INTO TariffsPCA ( PCA_ConfigID, PeriodID, PC1, PC2, PC3) values($configID, $(row.PeriodID), $(row.PC1), $(row.PC2), $(row.PC3))")
+end
+
+ModelWorker.closeDB(conn)
